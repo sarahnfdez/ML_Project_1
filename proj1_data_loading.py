156bfedb35b4b7719aef29544a7a9531cf39a33d
sarahnfdez <44241502+sarahnfdez@users.noreply.github.com>
2019-01-19 14:26:51 -0500

    finished task 1 (minus creating new features)
    -created 3 data sets
    -lowercased and tokenized text comments
    -found the top 160 most common words
    -created 160-dim sets marking common word frequency per comment (for training set)

--- proj1_data_loading.py
+++ proj1_data_loading.py
@@ -1,4 +1,5 @@
 import json # we need to use the JSON package to load the data, since the data is stored in JSON format
+import collections
 
 with open("proj1_data.json") as fp:
     data = json.load(fp)
@@ -14,19 +15,42 @@ with open("proj1_data.json") as fp:
 # Example:
 data_point = data[0] # select the first data point in the dataset
 
-#preprocessing the text
-for point in data:
-    point['text'] = point['text'].lower()
-    point['text'] = point['text'].split()
-
 # Now we print all the information about this datapoint
 for info_name, info_value in data_point.items():
     print(info_name + " : " + str(info_value))
 
+#preprocessing the data
+def preprocessing(dd):
+    for point in dd:
+        point['text'] = point['text'].lower().split()
+    return dd
 
+training = preprocessing(data[:10000])
+validation = preprocessing(data[10000:11000])
+testing = preprocessing(data[11000:12000])
 
-training = data[:10000]
-validation = data[10000:11000]
-testing = data[11000:12000]
+words = []
+for point in data:
+    text = point['text']
+    for word in text:
+            words.append(word)
+            
+most_comm = collections.Counter(words).most_common(160)
+most_common = []
+for tup in most_comm:
+    word = tup[0]
+    most_common.append(word)
 
-
+commonalities = []
+#creates a 160-dim array for every comment whose 
+#values are the number of times the frequent words are used
+for point in training:
+    xcounts = [0]*160
+    ind = 0
+    text = point['text']
+    for word1 in most_common:
+        for word2 in text:
+            if word2 == word1:
+                xcounts[ind] = xcounts[ind]+1
+        ind = ind+1
+    commonalities.append(xcounts)
