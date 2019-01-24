05297243c691c058b05ffc1144d98816ae98fb2d
mjip <marie.payne@hotmail.ca>
2019-01-24 11:44:35 -0500

    finished gradient descent, getting really weird answers but those are probably from hyperparameters

--- proj1_data_loading.py
+++ proj1_data_loading.py
@@ -48,12 +48,13 @@ class RedditComments:
 		self.processed_text = dd
 
 		# create data matrix X and output vector y
+		# the 1 represents x0 = 1, the bias term
 		initial_point = dd[0]
-		X = np.array([[initial_point['is_root'], initial_point['controversiality'], initial_point['children']]])
+		X = np.array([[1, initial_point['is_root'], initial_point['controversiality'], initial_point['children']]])
 		y = np.array([[initial_point['popularity_score']]])
 		for i in range(1, len(dd)):
 			point = dd[i]
-			X = np.concatenate((X, [[point['is_root'], point['controversiality'], point['children']]]), axis=0)
+			X = np.concatenate((X, [[1, point['is_root'], point['controversiality'], point['children']]]), axis=0)
 			y = np.concatenate((y, [[point['popularity_score']]]), axis=0)
 
 		return X, y
@@ -98,11 +99,46 @@ class RedditComments:
 		"""
 			Implements the closed-form solution w = (X^T X)^-1 X^T y, where X is
 			the data matrix and y is the output vector (popularity score vector)
+
+			Input: X - data matrix
+				   y - target vector
+			Output: w - weight vector (num features + 1 by 1)
 		"""
 		XT = X.transpose()
-		XTX_inverse = np.linalg.inv(XT.dot(X))
-		w = XTX_inverse.dot(XT).dot(y)
+		intermediate = np.linalg.inv(XT.dot(X))
+		w = intermediate.dot(XT).dot(y)
 		return w
+
+	def gradient_descent(self, X, y, w0, beta, n0, epsilon, maxiters):
+		"""
+			Steps through a function proportional to the negative of the gradient
+			to find its local minimum. We want to produce a sequence of weight solutions
+			such that the error is decreasing: Err(w0) > Err(w1) > Err(w2) ...
+			The n0 hyperparameter is the initial learning rate and the beta hyperparameter
+			controls the speed of the decay.
+
+			Input: X - data matrix
+				   y - target vector
+				   w0 - initial weight vector 
+				   beta, n0, epsilon - hyperparameters (beta is a vector)
+				   maxiters - the maximum number of iterations if it won't converge
+			Output: w - estimated weight vector (num features + 1 by 1)
+		"""
+		i = 1
+		w = [0] * maxiters
+		w[0] = w0
+		while True:
+			if i == maxiters:
+				i -= 1
+				break
+			alpha = n0 /(1 + beta[i])
+			XT = X.transpose()
+			w[i] = w[i-1] - (2 * alpha * (XT.dot(X).dot(w[i-1]) - XT.dot(y)))
+			print(w[i])
+			if np.linalg.norm(w[i] - w[i-1], ord=2) <= epsilon:
+				break
+			i += 1
+		return w[i]
 
 if __name__ == "__main__":
 	redcoms = RedditComments()
@@ -116,3 +152,8 @@ if __name__ == "__main__":
 	commonalities = redcoms.freq_dist()
 
 	w_train = redcoms.closed_form(X_train, y_train)
+	print(w_train)
+	w0 = np.array([[0],[0],[0],[0]])
+	b = [5] * 10
+	w_train = redcoms.gradient_descent(X_train, y_train, w0, b, 0.9, 0.01, 10)
+	print(w_train)
